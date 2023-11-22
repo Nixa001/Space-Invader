041c86d3f5783e363d7644594c5eb6eba55d3eae
igueye <iboug571@gmail.com>
2023-11-22 16:46:34 +0000

    refractor

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { Enemy } from "../controlers/enemy.js";
-import { move } from "../controlers/player/move.js";
-import { Players } from "../controlers/player/player.js";
-import { Background } from "./background.js";
+import { Enemy } from "./controlers/enemy/enemy.js";
+import { move } from "./controlers/player/move.js";
+import { Players } from "./controlers/player/player.js";
+import { Background } from "./vue/background.js";
 
 new Background();
 const elem = document.querySelector(".game-container");
 const player = new Players(elem);
-new Enemy(elem)
+new Enemy(elem);
 
 const keys = {
   ArrowLeft: false,
