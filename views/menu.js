44f964dba5fbef91c93257100bde38440b589d75
nifaye <nicolasfaye31@gmail.com>
2023-11-30 18:50:23 +0000

    style menu

--- views/menu.js
+++ views/menu.js
@@ -1,25 +1,35 @@
-// menu.js
 export class Menu {
     constructor(containerId, startCallback) {
-        this.container = document.getElementById(containerId);
-        this.startCallback = startCallback;
-        this.render();
+        this.container = document.createElement("div");
+        this.container.className = "game_menu";
+        this.container.id = "gameMenu";
+
+        this.header = document.createElement("h1");
+        this.header.innerText = "SPACE";
+
+        this.header2 = document.createElement("h2");
+        this.header2.innerText = "INVADER";
+        this.container.appendChild(this.header);
+        this.container.appendChild(this.header2);
+
+        this.startButton = document.createElement("button");
+        this.startButton.innerText = "Start Game";
+        this.startButton.addEventListener("click", () => {
+            if (typeof startCallback === "function") {
+                startCallback();
+            }
+        });
+        this.container.appendChild(this.startButton);
+
+        this.render(containerId);
     }
 
-    render() {
-        this.container.innerHTML = `
-        <div class="game_menu">    
-        <h1>SPACE INVADER</h1>
-        <button onclick="startGame()">Start Game</button>
-        </div>
-        `;
-        const startButton = this.container.querySelector("button");
-        if (startButton) {
-            startButton.addEventListener("click", () => {
-                if (typeof this.startCallback === "function") {
-                    this.startCallback();
-                }
-            });
+    render(containerId) {
+        const existingContainer = document.getElementById(containerId);
+        if (existingContainer) {
+            existingContainer.appendChild(this.container);
+        } else {
+            console.log(`Container with id ${containerId} not found.`);
         }
     }
 }
